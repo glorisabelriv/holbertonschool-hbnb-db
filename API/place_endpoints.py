c88c063597595d865bca2209ed66f854f84cd5f2
glorisabelriv <glorisabel200@gmail.com>
2024-07-23 22:29:09 -0400

    include information

--- API/place_endpoints.py
+++ API/place_endpoints.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, abort
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from Model.place import Place
+from Model.user import User
 from datab import db
 from Persistence.DataManager import DataManager
 
@@ -12,41 +13,49 @@ data_manager = DataManager()
 @place_bp.route('/places', methods=['POST'])
 @jwt_required()
 def create_place():
-    claims = get_jwt()
-    if not claims.get('is_admin'):
-        return jsonify({"msg": "Administration rights required"}), 403
-    
+    user = User.query.get(get_jwt_identity())
+    if not user.id == Place.query.get(user.id).host_id:
+        abort(403, description="owner already exists for this place")
+
     if not request.json:
         abort(400, description="Missing required fields")
 
-    data = request.json
+    if Place.query.filter_by(name=request.json.get('name', '')).first():
+        abort(409, description="Place already exists")
+
     place = Place(
-        name=data.get('name'),
-        description=data.get('description'),
-        address=data.get('address'),
-        city_id=data.get('city_id'),
-        latitude=data.get('latitude'),
-        longitude=data.get('longitude'),
-        host_id=data.get('host_id'),
-        number_of_rooms=data.get('number_of_rooms'),
-        number_of_bathrooms=data.get('number_of_bathrooms'),
-        price_per_night=data.get('price_per_night'),
-        max_guests=data.get('max_guests'),
-        amenity_ids=data.get('amenity_ids')
+        name=request.json.get('name', ''),
+        description=request.json.get('description', ''),
+        address=request.json.get('address', ''),
+        city_id=request.json.get('city_id', ''),
+        latitude=request.json.get('latitude', ''),
+        longitude=request.json.get('longitude', ''),
+        host_id=request.json.get('host_id', ''),
+        number_of_rooms=request.json.get('number_of_rooms', ''),
+        number_of_bathrooms=request.json.get('number_of_bathrooms', ''),
+        price_per_night=request.json.get('price_per_night', ''),
+        max_guests=request.json.get('max_guests', ''),
+        amenity_ids=request.json.get('amenity_ids', '')
     )
     db.session.add(place)
     db.session.commit()
+
     return jsonify(place.to_dict()), 201
 
 #GET /places
 @place_bp.route('/places', methods=['GET'])
+@jwt_required()
 def get_places():
+    user = User.query.get(get_jwt_identity())
+    if not user.is_admin:
+        abort(403, description="Admin rights required")
+
     places = Place.query.all()
-    return jsonify(places), 200
+    places_list = [place.to_dict() for place in places]
+    return jsonify(places_list), 200
 
 #GET /places/<place_id>
 @place_bp.route('/places/<place_id>', methods=['GET'])
-@jwt_required()
 def get_place(place_id):
     place = Place.query.get(place_id)
     if not place:
@@ -58,31 +67,26 @@ def get_place(place_id):
 @place_bp.route('/places/<place_id>', methods=['PUT'])
 @jwt_required()
 def update_place(place_id):
-    claims = get_jwt()
-    if not claims.get('is_admin'):
-        return jsonify({"msg": "Administration rights required"}), 403
-    
-    place = Place.query.get(place_id)
-    if not place:
-        abort(404, description="Place not found")
+    user = User.query.get(get_jwt_identity())
+    if not user.is_admin or not user.id == Place.query.get(place_id).host_id:
+        abort(403, description="Admin rights required or owner of the place to edit")
 
     if not request.json:
         abort(400, description="Missing required fields")
 
-    data = request.json
-    place.name = data.get('name', place.name)
-    place.description = data.get('description', place.description)
-    place.address = data.get('address', place.address)
-    place.city_id = data.get('city_id', place.city_id)
-    place.latitude = data.get('latitude', place.latitude)
-    place.longitude = data.get('longitude', place.longitude)
-    place.host_id = data.get('host_id', place.host_id)
-    place.number_of_rooms = data.get('number_of_rooms', place.number_of_rooms)
-    place.number_of_bathrooms = data.get('number_of_bathrooms', place.number_of_bathrooms)
-    place.price_per_night = data.get('price_per_night', place.price_per_night)
-    place.max_guests = data.get('max_guests', place.max_guests)
-    place.amenity_ids = data.get('amenity_ids', place.amenity_ids)
-    
+    place = Place.query.get(place_id)
+    if not place:
+        abort(404, description="Place not found")
+
+    if 'name' in request.json and Place.query.filter_by(name=request.json['name']).first():
+        abort(409, description="Place with given name already exists")
+
+    fields_to_update = ['name', 'description', 'address', 'city_id', 'latitude', 'longitude', 'host_id',
+                        'number_of_rooms', 'number_of_bathrooms', 'price_per_night', 'max_guests', 'amenity_ids']
+    for field in fields_to_update:
+        if field in request.json:
+            setattr(place, field, request.json[field])
+
     db.session.commit()
     return jsonify(place.to_dict()), 200
 
@@ -91,13 +95,14 @@ def update_place(place_id):
 @place_bp.route('/places/<place_id>', methods=['DELETE'])
 @jwt_required()
 def delete_place(place_id):
-    claims = get_jwt()
-    if not claims.get('is_admin'):
-        return jsonify({"msg": "Administration rights required"}), 403
-    
+    user = User.query.get(get_jwt_identity())
+    if not user.is_admin or not user.id == Place.query.get(place_id).host_id:
+        abort(403, description="Admin rights required or owner of the place to delete")
+
     place = Place.query.get(place_id)
     if not place:
         abort(404, description="Place not found")
+
     db.session.delete(place)
     db.session.commit()
-    return '', 204
+    return jsonify({"message": "Place deleted successfully"}), 200
