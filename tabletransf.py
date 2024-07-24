a5f0990d8c91707f862e9694b72c668867529797
glorisabelriv <glorisabel200@gmail.com>
2024-07-23 21:59:14 -0400

    include information

--- tabletransf.py
+++ tabletransf.py
@@ -5,19 +5,18 @@ import uuid
 connect = sqlite3.connect('instance/development.db')
 cursor = connect.cursor()
 
-
 with open('countries.json', 'r') as file:
     countries = json.load(file)
 
 for country in countries:
-    cursor.execute("SELECT * FROM countries WHERE code= ?", (country['code'],))
+    cursor.execute("SELECT * FROM country WHERE code=?", (country['code'],))
     data = cursor.fetchone()
     if data is None:
         countries_id = str(uuid.uuid4())
         cursor.execute(
-            "INSERT INTO countries (id, name, code) VALUES (?, ?, ?);",
-             (countries_id, country['name'], country['code'])
-    )
+            "INSERT INTO country (id, name, code) VALUES (?, ?, ?);",
+            (countries_id, country['name'], country['code'])
+        )
 
 connect.commit()
 cursor.close()
